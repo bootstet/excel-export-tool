3b100bff646131268e1df6ce33eb4cee1089354e
bootstet <bootstet@163.com>
2024-07-21 16:57:32 +0800

    feat: 客户端

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,141 @@
+'use client';
 import Image from "next/image";
+import { Upload, Button, message } from 'antd';
+import { UploadOutlined } from '@ant-design/icons';
+import * as XLSX from 'xlsx';
+import axios from 'axios'
 
 export default function Home() {
+  // 获取SKC值
+  const findStringAndNextWord = (text, searchString) => {
+    const regex = new RegExp(`${searchString}\\s(\\w+)`);
+    const match = text.match(regex);
+    if (match) {
+        return match[1]; // 返回第一个括号中匹配的单词
+    }
+    return null; // 如果没有匹配，返回null
+  }
+   
+   // 上传文件并解析成json
+   
+   const HandleImportFile = (info) => {
+    const files = info.file;
+    // 获取文件名称
+    const name = files.name;
+    // 获取文件后缀
+    const suffix = name.substr(name.lastIndexOf('.'));
+    const reader = new FileReader();
+    reader.onload = async(event) => {
+      try {
+        // 判断文件类型是否正确
+        if ('.xls' != suffix && '.xlsx' != suffix) {
+          message.error('选择Excel格式的文件导入!');
+          return false;
+        }
+        const { result } = event.target;
+        // 读取文件
+        const workbook = XLSX.read(result, { type: 'binary' });
+        let data = [];
+        // 循环文件中的每个表
+        console.log('表格数据workbook：', workbook);
+        for (const sheet in workbook.Sheets) {
+          if (workbook.Sheets.hasOwnProperty(sheet)) {
+            // 将获取到表中的数据转化为json格式
+            data = data.concat(XLSX.utils.sheet_to_json(workbook.Sheets[sheet]));
+          }
+        }
+        console.log('原始表格数据', data)
+
+      
+
+        if (data && data.length < 1) {
+          message.error('表格中没有数据,请重新上传');
+          return;
+        }
+        if (data && data.length > 0) {
+          const result = data.reduce((acc, cur) => {
+            const key = findStringAndNextWord(cur['商品信息'], 'SKC:')
+            acc[key] = cur['数量']
+            return acc
+          }, {})
+          console.log('result', result)
+          info.onProgress({ percent: 100 }, info.file);
+          info.onSuccess(info.res, info.file);
+          axios.get('http://localhost:3001/getData', {
+            params: result,
+            responseType: 'blob',
+            headers: {
+              responseType: 'blob'
+            }
+          })
+          .then(function (response) {
+            console.log(response);
+            let blob = new Blob([response.data], { type: "application/zip" })
+            let url = window.URL.createObjectURL(blob)
+            const link = document.createElement("a")
+            link.href = url
+            link.download = '下单商品图片及数量.zip' // 重命名文件
+            link.click()
+            URL.revokeObjectURL(url) // 释放内存
+          })
+          .catch(function (error) {
+            console.log(error);
+          })
+          .finally(function () {
+            // always executed
+          });
+        }
+      } catch (e) {
+        console.error('e', e)
+        message.error('文件类型不正确！');
+      }
+    };
+    reader.readAsBinaryString(files);
+  };
+  const props = {
+    name: 'file',
+    action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+    headers: {
+      authorization: 'authorization-text',
+    },
+    onChange(info) {
+      if (info.file.status !== 'uploading') {
+        console.log(info.file, info.fileList);
+      }
+      // const result = info.file.raw;
+      // const workbook = XLSX.read(result, { type: 'binary' });
+      if (info.file.status === 'done') {
+        message.success(`${info.file.name} file uploaded successfully`);
+      } else if (info.file.status === 'error') {
+        message.error(`${info.file.name} file upload failed.`);
+      }
+    },
+
+    customRequest: HandleImportFile,
+  };
+  const readImageFile = async() => {
+    const res = await window.showOpenFilePicker({
+      types: [
+        {
+            description: 'Not JavaScript',
+            accept: {
+                'image/*': ['.py', '.js', '.html']
+            },
+        },
+      ],
+      // excludeAcceptAllOption: true,
+      multiple: true,
+      id: "thisGonnaBeDDrive" 
+    })
+    console.log('选取的图片文件信息', res)
+  }
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main className="flex min-h-screen  justify-between p-24">
+        <Upload {...props}>
+        <Button type="primary" icon={<UploadOutlined />}>
+          导入表格
+        </Button>
+      </Upload>
     </main>
   );
 }
